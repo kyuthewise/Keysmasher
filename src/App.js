dab44f29f82731c5bc249187c81f8e201b829add
kyuthewise <91840641+kyuthewise@users.noreply.github.com>
2023-06-24 23:47:25 +0200

    l

--- src/App.js
+++ src/App.js
@@ -1,122 +1,111 @@
-import React from 'react';
-import {useState, useEffect} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-
-
 function App() {
+  const [words, setWords] = useState([]);
+  const [numVal, setNumVal] = useState(0);
+  const [inputValue, setInputValue] = useState('');
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [completedWords, setCompletedWords] = useState([]);
+  const [typedLetters, setTypedLetters] = useState('');
+  const [visible, setVisible] = useState(false);
+  const inputRef = useRef(null);
 
 
-  let index = 0;
-  const[words, setWords] = useState([]);
-  const[numVal, setnumVal] = useState(0);
-const [inputValue, setInputValue] = useState('');
-const [currentWordIndex, setCurrentWordIndex] = useState(0);
-const handleChange = (event) => {
-  const typedValue = event.target.value;
-  setInputValue(typedValue);
 
-  // Check if the typed value matches the current word up to the typed length
-  if (typedValue === words[currentWordIndex].slice(0, typedValue.length)) {
-    setTypedLetters(typedValue);
+  const handleChange = (event) => {
 
-    // Check if the entire word has been typed correctly
-    if (typedValue === words[currentWordIndex]) {
-      setCurrentWordIndex((prevIndex) => prevIndex + 1);
-      setTypedLetters('');
-      console.log('yuh');
-      setnumVal(numVal + 1);
-      
-      setInputValue('');
+    if (words.length === 0 || currentWordIndex >= words.length) {
+      return;
     }
-  } else {
-    setTypedLetters(typedValue);
-  }
-};
+    const typedValue = event.target.value;
+    setInputValue(typedValue);
+    console.log(typedValue)
+    // Check if the typed value matches the current word up to the typed length
+    if (typedValue === words[currentWordIndex].slice(0, typedValue.length)) {
+      setTypedLetters(typedValue);
+  
+      // Check if the entire word has been typed correctly
+      if (typedValue === words[currentWordIndex]) {
+        setCurrentWordIndex((prevIndex) => prevIndex + 1);
+        setTypedLetters('');
+        setNumVal((prevNumVal) => prevNumVal + 1);
+        setInputValue('');
+      }
+    } else {
+      setTypedLetters(typedValue);
+    }
+  };
 
-console.log(numVal);
-const [completedWords, setCompletedWords] = useState([]);
-const[typedLetters, setTypedLetters] = useState('');
-const Bloo = () => {
-const[index, setIndex] = useState(0);
 
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      
+      if (event.key === 'Backspace') {
 
+        setInputValue((prevInputValue) => {
+          const updatedValue = prevInputValue.slice(0, -1);
+          handleChange({ target: { value: updatedValue } });
 
- }
-
-
-Bloo();
-
+        
+      }); }
+      else {
+        const typedValue = inputValue + event.key;
+        setInputValue(typedValue);
+        handleChange({ target: { value: typedValue } });
+    };
+  };
+    window.addEventListener('keydown', handleKeyDown);
+  
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [inputValue]);
 
   const fetchUserData = () => {
-    fetch("https://random-word-api.vercel.app/api?words=25")
-    .then(response => {
-      return response.json()
-    }).then (data =>{
-      setWords(data)
-    })
-  }
-
+    fetch('https://random-word-api.vercel.app/api?words=25')
+      .then((response) => response.json())
+      .then((data) => {
+        setWords(data);
+      });
+  };
 
   useEffect(() => {
-    fetchUserData()
-  }, [])
-  
-  const [visibl, setVisible] = useState(false);
+    fetchUserData();
+  }, []);
 
-  
   useEffect(() => {
-  if (numVal == words.length){
-    console.log('compls');
-    setVisible(true);
-  
-  }
-  else{
-    setVisible(false)
-  }
-  
-  }, [numVal, words])
-  console.log(visibl)
-
-  console.log(words.length)
+    setVisible(numVal === words.length);
+  }, [numVal, words]);
+console.log(inputValue);
 
 
 
   return (
-<div className='Conts'>
-<div className ="textA">
-<ul className='ulize'>
-{words.map((word, i) => (
-  <li className="dalist" key={i}>
-    {word.split('').map((letter, j) => {
-      const isTyped = i < currentWordIndex || (i === currentWordIndex && j < typedLetters.length);
-      const isCorrect = isTyped && letter === typedLetters[j];
-      const isCompletedWord = i < currentWordIndex;
-      const letterClass = isCompletedWord ? 'completed2' : isCorrect ? 'typed' : isTyped ? 'incorrect' : '';
-      return (
-        <span
-          key={j}
-          className={`letter ${letterClass} `}
-        >
-          {letter}
-        </span>
-      );
-    })}
-  </li>
-))}
-
-
-
- </ul>
- {visibl && <p class="pew">You got all the words!</p>}
- <input className="inp" type="text" value={inputValue} onChange={handleChange}></input>
-
-
-</div>
-</div>
-    
-    
+    <div className="Conts">
+      <div className="textA">
+        <ul className="ulize">
+          {words.map((word, i) => (
+            <li className="dalist" key={i}>
+              {word.split('').map((letter, j) => {
+                const isTyped = i < currentWordIndex || (i === currentWordIndex && j < typedLetters.length);
+                const isCorrect = isTyped && letter === typedLetters[j];
+                const isCompletedWord = i < currentWordIndex;
+                const letterClass = isCompletedWord ? 'completed2' : isCorrect ? 'typed' : isTyped ? 'incorrect' : '';
+                return (
+                  <span key={j} className={`letter ${letterClass} `}>
+                    {letter}
+                  </span>
+                );
+              })}
+            </li>
+          ))}
+        </ul>
+        {visible && <p className="pew">You got all the words!</p>}
+        <input className="inp" type="text" onKeyDown={handleChange} />
+      </div>
+    </div>
   );
 }
 
